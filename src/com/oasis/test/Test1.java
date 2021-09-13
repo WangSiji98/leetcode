04605b44e5709ce44db830b8c79560caf222b246
Siji <807403713@qq.com>
2021-09-13 00:31:03 -0700

    9.12

--- src/com/oasis/test/Test1.java
+++ src/com/oasis/test/Test1.java
@@ -1,5 +1,14 @@
 package com.oasis.test;
 
 public class Test1 {
+    public static void main(String[] args) {
+        byte a = 0;
+        int[] cells = new int[]{0 , 1, 1, 1, 0, 1, 0 ,1};
+        for (int i = 0; i < 8; ++i) {
+            System.out.println((cells[i] & 1) << i);
+            a ^= ((cells[i] & 1) << i);
+        }
+        System.out.println(a);
+    }
 
 }
